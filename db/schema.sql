830b5dd47dab14da124cef76eb90ec1667c7eb41
Paden Pace <pjpace27@yahoo.com>
2017-05-15 21:28:57 -0500

    files from original group arcade

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE arcade_solo_db;
+
+USE arcade_solo_db;
+
+CREATE TABLE players_scores(
+    id INT NOT NULL AUTO_INCREMENT,
+    playerName VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    simonHiScore INT(10) NOT NULL DEFAULT 0,
+    pogNumOfWins INT(10) NOT NULL DEFAULT 0,
+    blackJackHiScore INT(10) NOT NULL DEFAULT 0,
+    rpsNumOfWins INT(10) NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
